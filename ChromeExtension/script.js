a193bd9f5485840131fb6686b8ea599520aa8b57
JC-MT <jancarlosmatias@gmail.com>
2022-11-05 19:52:08 -0400

    update readme

--- ChromeExtension/script.js
+++ ChromeExtension/script.js
@@ -1,5 +1,3 @@
-console.log("This is a popup!")
-
 let storage = JSON.parse(localStorage.getItem('link'))
 
 if(storage){
@@ -14,34 +12,33 @@ if(storage){
         anchor.textContent = `Item Link for ${recipient}`
 
         newListItem.append(anchor)
-
         document.querySelector(".list-links").append(newListItem)
     })
 }
 
-
 chrome.tabs.query(
     {active: true, lastFocusedWindow: true}, tabs => {
     let url = tabs[0].url;
+    
     document.querySelector("button").addEventListener('click', function(){
-        console.log(document.querySelector(".input-value").value)
         getLocalStorage(url)
     })
 });
 
 document.querySelector('button.delete-URL').addEventListener('click', function(){
     localStorage.clear()
+
     let el = document.querySelector('ul')
     let child = document.querySelectorAll('li')
+
     el.removeChild(child[child.length - 1])
 })
-
 
 function getLocalStorage(url){
 
     let recipient = document.querySelector(".input-value").value
-
     const oldStorage = JSON.parse(localStorage.getItem('link'))
+
     if(oldStorage){
         localStorage.setItem('link', JSON.stringify([...oldStorage, {link: url, for: recipient}]));
     } else {
@@ -57,7 +54,5 @@ function getLocalStorage(url){
     anchor.textContent = `Item Link for ${recipient}`
 
     newListItem.append(anchor)
-
     document.querySelector(".list-links").append(newListItem)
-
   };
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # MLH-Pursuit-2022
+
+## Mall Santa's Wish List
+
+This Chrome Extension project allows users to save a list of gifts that correspond to a recipient in their family. This project uses LocalStorage to create persistant user data. When the season is over, you can delete your wish list or simply allow it to exist for next year.
+
+
+### Group Members
+- [Candace Garvin](https://github.com/CandaceNia)
+- [Pratima Roy](https://github.com/PratimaRoy)
+- [Hermanclyde Guerrero](https://github.com/HC-Guerrero)
+- [Jan C. Matias](https://github.com/JC-MT)
